809c41389999ac4732c8d382ed1dcc5c93d7fbb4
BreakingBran <lancepereira99@gmail.com>
2017-04-21 00:32:05 -0400

    got basic gui working

--- src/testingGui/Main.java
+++ src/testingGui/Main.java
@@ -1,34 +1,69 @@
 package testingGui;
 
+import javafx.event.ActionEvent;
+
 import com.sun.scenario.effect.impl.prism.PrImage;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 
-public class Main extends Application{
-	
+public class Main extends Application {
+
 	Button button;
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
 
 	@Override
-	public void start(Stage arg0) throws Exception {
+	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub ASas
-		arg0.setTitle("Banking System");
+		primaryStage.setTitle("Banking System");
 		button = new Button();
 		button.setText("Create New user");
-		
+		button.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event){
+				System.out.println("I exist");
+			}
+		});
+
 		StackPane layout = new StackPane();
 		layout.getChildren().add(button);
-		
-		Scene scene = new Scene(layout,300,250);
-		arg0.setScene(scene);
-		
-		arg0.show();
+
+		Scene scene = new Scene(layout, 300, 250);
+		primaryStage.setScene(scene);
+
+		primaryStage.show();
 	}
+
 }
+
+/*
+ * public class Main extends Application implements EventHandler<ActionEvent>{
+ * 
+ * Button button;
+ * 
+ * public static void main(String[] args) { launch(args); }
+ * 
+ * @Override public void start(Stage primaryStage) throws Exception { // TODO
+ * Auto-generated method stub ASas primaryStage.setTitle("Banking System");
+ * button = new Button(); button.setText("Create New user");
+ * button.setOnAction(this);
+ * 
+ * 
+ * StackPane layout = new StackPane(); layout.getChildren().add(button);
+ * 
+ * 
+ * Scene scene = new Scene(layout,300,250); primaryStage.setScene(scene);
+ * 
+ * primaryStage.show(); }
+ * 
+ * @Override public void handle(ActionEvent event) { // TODO Auto-generated
+ * method stub if(event.getSource() == button) {
+ * System.out.println("Will create new account"); } } }
+ */
