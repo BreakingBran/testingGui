085b6f095536ae5d44dab5bffc3d0e696b1e2900
BreakingBran <lancepereira99@gmail.com>
2017-04-20 23:36:20 -0400

    First attempts at gui

---
+++ src/testingGui/Main.java
@@ -0,0 +1,34 @@
+package testingGui;
+
+import com.sun.scenario.effect.impl.prism.PrImage;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+
+public class Main extends Application{
+	
+	Button button;
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		arg0.setTitle("Banking System");
+		button = new Button();
+		button.setText("Create New user");
+		
+		StackPane layout = new StackPane();
+		layout.getChildren().add(button);
+		
+		Scene scene = new Scene(layout,300,250);
+		arg0.setScene(scene);
+		
+		arg0.show();
+	}
+}
